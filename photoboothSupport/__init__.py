e18fb976fcae3d2ef18d1fd022dc02094e07c2bf
Angel Arroyo <angelitov7@gmail.com>
2018-02-22 22:20:04 -0500

    Updated path

--- photoboothSupport/__init__.py
+++ photoboothSupport/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import json
 
-def loadJson(jsonFilePath="../res/configuration.json"):
+def loadJson(jsonFilePath="./res/configuration.json"):
     '''Reads in the configuraiton json file that has the applications parameters'''
     fileContent = file(jsonFilePath, "r")
     jsonFile = json.load(fileContent)
